cb1bb5f943daf68bc0a536cf97557ec32f65c642
Geoffrey Chancel <geoffrey.chancel@outlook.com>
2023-07-28 14:09:10 +0200

    Chapter 2 almost complete

--- 2_goodPractices/figures/pulse+ignd_forManuscript.py
+++ 2_goodPractices/figures/pulse+ignd_forManuscript.py
@@ -49,14 +49,23 @@ idealPulse[bornesAll] = -140
 beginBad = np.where(np.isclose(badGND['iTime'], impGND['iTime'][0], atol = 100e-12))[0][0]
 endBad = np.where(np.isclose(badGND['iTime'], impGND['iTime'][-1], atol = 100e-12))[0][-1]
 
+badGND['iTime'] = badGND['iTime'][beginBad:endBad]
+badGND['vTime'] = badGND['vTime'][beginBad:endBad]
+
+badGND['iTime'][:] -= badGND['iTime'].min()
+badGND['vTime'][:] -= badGND['vTime'].min()
+
+impGND['iTime'][:] -= impGND['iTime'].min()
+impGND['vTime'][:] -= impGND['vTime'].min()
+
 rmsBad = np.sqrt(np.mean(np.square(badGND['iGND'][beginBad:endBad])))
 # rmsGood = np.sqrt(np.mean(np.square(goodGND['iGND'])))
 rmsImp = np.sqrt(np.mean(np.square(impGND['iGND'])))
 
 
-scale = 1.2
+scale = 1.0
 lw = 2
-labSize = 11
+labSize = 10
 fSize = 12 * 0.9
 ySize = 13 * scale
 titleSize = 14 * scale
@@ -72,7 +81,7 @@ supSize = 20
 yLabelRot = 90
 textposX = 0.99
 textposY = 0.01
-offset = 0.18
+offset = 0.18 / 2
 
 # subAdj = {'top': 0.915,
 #           'bottom': 0.031,
@@ -81,59 +90,63 @@ offset = 0.18
 #           'hspace': 0.745,
 #           'wspace': 0.200}
 
-fig, ax = plt.subplots(4, figsize = (5.0 * scale, 11 * 0.67 * scale), sharex = True)
+# fig, ax = plt.subplots(4, figsize = (5.0 * scale, 11 * 0.67 * scale), sharex = True)
+fig, ax = plt.subplots(2, 2, figsize = (12 * scale, 7 * scale), sharex = True)
 fig.suptitle("BBI practices comparison", size = supSize * scale, bbox = sBox, y = 0.99)
 
-ax[1].plot(badGND['iTime'][beginBad:endBad], badGND['iGND'][beginBad:endBad],
-           lw = lw * scale)
-ax[1].yaxis.set_major_formatter(form_amp)
-ax[1].xaxis.set_major_formatter(form_sec)
-ax[1].text(textposX,
-           textposY,
-           r'RMS $I_{GND}$ = ' + f'{rmsBad * 1000:.2f} mA',
-           verticalalignment = 'bottom', horizontalalignment = 'right',
-           transform = ax[1].transAxes,
-           fontsize = fSize * scale)
-ax[1].set_ylabel('$I_{GND}(t)$', fontsize = ySize, labelpad = yLabelPad, bbox = tBox,
-                 rotation = yLabelRot)
-# ax[1].vlines(x = 20e-9,
-#              ymin = badGND['iGND'][beginBad:endBad].min() * 1.0,
-#              ymax = badGND['iGND'][beginBad:endBad].max() * 1.0,
-#              color = 'red',
-#              lw = 1)
+ax[0, 1].plot(badGND['iTime'], badGND['iGND'][beginBad:endBad],
+              lw = lw * scale)
+ax[0, 1].yaxis.set_major_formatter(form_amp)
+ax[0, 1].xaxis.set_major_formatter(form_sec)
+ax[0, 1].text(textposX,
+              textposY,
+              r'RMS $I_{GND}$ = ' + f'{rmsBad * 1000:.2f} mA',
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[0, 1].transAxes,
+              fontsize = fSize * scale)
+ax[0, 1].set_ylabel('$I_{GND}(t)$', fontsize = ySize, labelpad = yLabelPad, bbox = tBox,
+                    rotation = yLabelRot)
+ax[0, 1].set_title("State of the art grounding",
+                   fontsize = titleSize, bbox = tBox, pad = tPad, loc = tLoc0)
 
-ax[0].plot(badGND['vTime'][beginBad:endBad], badGND['vPulse'][beginBad:endBad],
-           lw = lw * scale)
-ax[0].yaxis.set_major_formatter(form_volt)
-ax[0].xaxis.set_major_formatter(form_sec)
-ax[0].set_title("State of the art grounding",
-                fontsize = titleSize, bbox = tBox, pad = tPad, loc = tLoc0)
-ax[0].text(textposX,
-           textposY,
-           f"Max. abs. voltage = {np.amin(badGND['vPulse'][beginBad:endBad]):.1f} V",
-           verticalalignment = 'bottom', horizontalalignment = 'right',
-           transform = ax[0].transAxes,
-           fontsize = fSize * scale)
-ax[0].text(textposX,
-           textposY + offset,
-           "Set-point voltage = -140 V",
-           verticalalignment = 'bottom', horizontalalignment = 'right',
-           transform = ax[0].transAxes,
-           fontsize = fSize * scale)
-ax[0].text(textposX,
-           textposY + offset * 2.0,
-           r"Rise time $\approx$ 65 ns",
-           verticalalignment = 'bottom', horizontalalignment = 'right',
-           transform = ax[0].transAxes,
-           fontsize = fSize * scale)
-ax[0].text(textposX,
-           textposY + offset * 3.0,
-           r"Fall time $\approx$ 16 ns",
-           verticalalignment = 'bottom', horizontalalignment = 'right',
-           transform = ax[0].transAxes,
-           fontsize = fSize * scale)
-ax[0].set_ylabel('$V_{PULSE}(t)$', fontsize = ySize, labelpad = yLabelPad, bbox = tBox,
-                 rotation = yLabelRot)
+ax[0, 0].plot(badGND['vTime'], badGND['vPulse'][beginBad:endBad],
+              lw = lw * scale)
+ax[0, 0].yaxis.set_major_formatter(form_volt)
+ax[0, 0].xaxis.set_major_formatter(form_sec)
+ax[0, 0].set_title("State of the art grounding",
+                   fontsize = titleSize, bbox = tBox, pad = tPad, loc = tLoc0)
+ax[0, 0].text(textposX,
+              textposY,
+              f"Max. abs. voltage = {np.amin(badGND['vPulse'][beginBad:endBad]):.1f} V",
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[0, 0].transAxes,
+              fontsize = fSize * scale)
+ax[0, 0].text(textposX,
+              textposY + offset,
+              "Set-point voltage = -140 V",
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[0, 0].transAxes,
+              fontsize = fSize * scale)
+ax[0, 0].text(textposX,
+              textposY + offset * 2.0,
+              r"Rise time $\approx$ 65 ns",
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[0, 0].transAxes,
+              fontsize = fSize * scale)
+ax[0, 0].text(textposX,
+              textposY + offset * 3.0,
+              r"Fall time $\approx$ 16 ns",
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[0, 0].transAxes,
+              fontsize = fSize * scale)
+ax[0, 0].text(textposX,
+              textposY + offset * 4.0,
+              r"Pulse width $\approx$ 75 ns",
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[0, 0].transAxes,
+              fontsize = fSize * scale)
+ax[0, 0].set_ylabel('$V_{PULSE}(t)$', fontsize = ySize, labelpad = yLabelPad, bbox = tBox,
+                    rotation = yLabelRot)
 # ax[0].get_shared_x_axes().join(ax[0], ax[1])
 # ax[1].get_shared_x_axes().join(ax[1], ax[0])
 
@@ -199,52 +212,53 @@ ax[0].set_ylabel('$V_{PULSE}(t)$', fontsize = ySize, labelpad = yLabelPad, bbox 
 #              color = 'red',
 #              lw = 1)
 
-ax[3].plot(impGND['iTime'], impGND['iGND'],
-           lw = lw * scale)
-ax[3].yaxis.set_major_formatter(form_amp)
-ax[3].xaxis.set_major_formatter(form_sec)
-ax[3].text(textposX,
-           textposY,
-           r'RMS $I_{GND}$ = ' + f'{round(rmsImp * 1000, 2)} mA',
-           verticalalignment = 'bottom', horizontalalignment = 'right',
-           transform = ax[3].transAxes,
-           fontsize = fSize * scale)
-ax[3].set_ylabel('$I_{GND}(t)$', fontsize = ySize, labelpad = yLabelPad, bbox = tBox,
-                 rotation = yLabelRot)
-# ax[3].vlines(x =6 1)
+ax[1, 1].plot(impGND['iTime'], impGND['iGND'],
+              lw = lw * scale)
+ax[1, 1].yaxis.set_major_formatter(form_amp)
+ax[1, 1].xaxis.set_major_formatter(form_sec)
+ax[1, 1].text(textposX,
+              textposY,
+              r'RMS $I_{GND}$ = ' + f'{round(rmsImp * 1000, 2)} mA',
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[1, 1].transAxes,
+              fontsize = fSize * scale)
+ax[1, 1].set_ylabel('$I_{GND}(t)$', fontsize = ySize, labelpad = yLabelPad, bbox = tBox,
+                    rotation = yLabelRot)
+ax[1, 1].set_title("Ground bypass and impedance matching",
+                   fontsize = titleSize, bbox = tBox, pad = tPad, loc = tLoc2)
 
-ax[2].plot(impGND['vTime'], impGND['vPulse'],
-           lw = lw * scale)
-ax[2].yaxis.set_major_formatter(form_volt)
-ax[2].xaxis.set_major_formatter(form_sec)
-ax[2].set_title("Ground bypass and impedance matching",
-                fontsize = titleSize, bbox = tBox, pad = tPad, loc = tLoc2)
-ax[2].text(textposX,
-           textposY,
-           f"Max. abs. voltage = {np.amin(impGND['vPulse']):.1f} V",
-           verticalalignment = 'bottom', horizontalalignment = 'right',
-           transform = ax[2].transAxes,
-           fontsize = fSize * scale)
-ax[2].text(textposX,
-           textposY + offset,
-           "Set-point voltage = -140 V",
-           verticalalignment = 'bottom', horizontalalignment = 'right',
-           transform = ax[2].transAxes,
-           fontsize = fSize * scale)
-ax[2].text(textposX,
-           textposY + offset * 2.0,
-           r"Rise and fall time $\approx$ 16 ns",
-           verticalalignment = 'bottom', horizontalalignment = 'right',
-           transform = ax[2].transAxes,
-           fontsize = fSize * scale)
-ax[2].text(textposX,
-           textposY + offset * 3.0,
-           r"Pulse width $\approx$ 20 ns",
-           verticalalignment = 'bottom', horizontalalignment = 'right',
-           transform = ax[2].transAxes,
-           fontsize = fSize * scale)
-ax[2].set_ylabel('$V_{PULSE}(t)$', fontsize = ySize, labelpad = yLabelPad, bbox = tBox,
-                 rotation = yLabelRot)
+ax[1, 0].plot(impGND['vTime'], impGND['vPulse'],
+              lw = lw * scale)
+ax[1, 0].yaxis.set_major_formatter(form_volt)
+ax[1, 0].xaxis.set_major_formatter(form_sec)
+ax[1, 0].set_title("Ground bypass and impedance matching",
+                   fontsize = titleSize, bbox = tBox, pad = tPad, loc = tLoc2)
+ax[1, 0].text(textposX,
+              textposY,
+              f"Max. abs. voltage = {np.amin(impGND['vPulse']):.1f} V",
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[1, 0].transAxes,
+              fontsize = fSize * scale)
+ax[1, 0].text(textposX,
+              textposY + offset,
+              "Set-point voltage = -140 V",
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[1, 0].transAxes,
+              fontsize = fSize * scale)
+ax[1, 0].text(textposX,
+              textposY + offset * 2.0,
+              r"Rise and fall time $\approx$ 16 ns",
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[1, 0].transAxes,
+              fontsize = fSize * scale)
+ax[1, 0].text(textposX,
+              textposY + offset * 3.0,
+              r"Pulse width $\approx$ 20 ns",
+              verticalalignment = 'bottom', horizontalalignment = 'right',
+              transform = ax[1, 0].transAxes,
+              fontsize = fSize * scale)
+ax[1, 0].set_ylabel('$V_{PULSE}(t)$', fontsize = ySize, labelpad = yLabelPad, bbox = tBox,
+                    rotation = yLabelRot)
 # ax[2].vlines(x = 12e-9,
 #              ymin = impGND['vPulse'].min() * 1.0,
 #              ymax = impGND['vPulse'].max() * 1.0,
@@ -252,14 +266,14 @@ ax[2].set_ylabel('$V_{PULSE}(t)$', fontsize = ySize, labelpad = yLabelPad, bbox 
 #              lw = 1)
 
 counter = 0
-for axf in ax:
+for axf in ax.ravel():
     axf.set_xlabel("Time", fontsize = ySize, bbox = xBox)
     axf.tick_params(axis = 'both', which = 'both', reset = True,
                     labelsize = labSize * scale)
     axf.yaxis.set_label_position("right")
     axf.text(0.05,
              0.05,
-             f"S{counter//2 + 1}",
+             f"S{counter//2 + 1}{'P' if counter % 2 == 0 else 'G'}",
              verticalalignment = 'bottom', horizontalalignment = 'left',
              transform = axf.transAxes,
              fontsize = fSize * 1.5 * scale,
@@ -267,6 +281,7 @@ for axf in ax:
     counter += 1
 
 fig.tight_layout()
+fig.show()
 
 try:
     fig.subplots_adjust(top = subAdj['top'],
@@ -286,3 +301,5 @@ PhD_thesis_Chancel/2_goodPractices/figures/"
 
 plt.savefig(f"{saveDir}realPulsesComparisons.pdf",
             transparent = False)
+
+fig.canvas.manager.window.raise_()
